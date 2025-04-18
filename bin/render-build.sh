d06d5133fece47f6ae1ab9746fac008e97aeccfb
SarazaToussaint <tmsaraza@gmail.com>
2025-04-18 10:45:46 +0200

    Render environment set up

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,5 +2,11 @@
 set -o errexit
 
 bundle install
+
+# Run database setup
+bundle exec rails db:migrate
+bundle exec rails db:seed
+
+# Precompile assets
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
